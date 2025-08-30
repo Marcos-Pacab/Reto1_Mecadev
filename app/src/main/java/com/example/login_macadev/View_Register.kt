3b3a0679603b41510b383e0aa127aad242541172
Marcos_Pacab <marcospacabmay@gmail.com>
2025-08-29 19:58:02 -0600

    1er version

---
+++ app/src/main/java/com/example/login_macadev/View_Register.kt
@@ -0,0 +1,30 @@
+package com.example.login_macadev
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.login_macadev.databinding.ActivityViewRegisterBinding
+
+class View_Register : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityViewRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityViewRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_view_register)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
